2a65ab416aee94ce3117fcbe281e0481e0462f7b
Waleed <waleedffx@gmail.com>
2022-01-31 11:55:46 -0600

    Day43- Honda

--- src/January/day43_Abstraction/car/Honda.java
+++ src/January/day43_Abstraction/car/Honda.java
@@ -1,5 +1,13 @@
 package January.day43_Abstraction.car;
 
-public class Honda {
+public class Honda extends Car {
+    public Honda(String brand, String model, String color, int year, double price) {
+        super(brand, model, color, year, price);
+    }
+
+
+
+
+
 
 }
