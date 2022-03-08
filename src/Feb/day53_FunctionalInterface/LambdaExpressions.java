5f2d9fda2cd370a3b061c87c0933cc09d2556880
Waleed <waleedffx@gmail.com>
2022-03-07 19:50:11 -0600

    Day 53 - Functional Interface - Stream

--- src/Feb/day53_FunctionalInterface/LambdaExpressions.java
+++ src/Feb/day53_FunctionalInterface/LambdaExpressions.java
@@ -3,7 +3,14 @@ package Feb.day53_FunctionalInterface;
 public class LambdaExpressions {
     public static void main(String[] args) {
 
+        MyThirdFunctionalInterface<String> stringReverse = (s) -> {
+            String reverse = new StringBuilder(s).reverse().toString();
+            return reverse;
+        };
 
+        String result = stringReverse.method("Wooden spoon");
+
+        System.out.println("result = " + result);
 
 
 
